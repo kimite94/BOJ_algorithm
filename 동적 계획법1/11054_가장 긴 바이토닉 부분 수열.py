6174413082a1733ddf24b59191d567ca71f4f000
kimite94 <72334494+kimite94@users.noreply.github.com>
2021-05-25 20:51:32 +0900

    Add files via upload

---
+++ 동적 계획법1/11054_가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+a_list = [1 for i in range(n)]
+b_list = [1 for i in range(n)]
+result = [0 for i in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            a_list[i] = max(a_list[i], a_list[j]+1)
+
+for i in range(n-1,-1,-1):
+    for j in range(n-1,i,-1):
+        if arr[i] > arr[j]:
+            b_list[i] = max(b_list[i], b_list[j]+1)
+
+    result[i] = a_list[i] + b_list[i]-1
+
+print(max(result))
