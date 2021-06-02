62fe2894c28affd45754df216f5eddd3f5175d3a
kimite94 <72334494+kimite94@users.noreply.github.com>
2021-06-02 21:25:57 +0900

    Update 2156_포도주 시식.py

--- 동적 계획법1/2156_포도주 시식.py
+++ 동적 계획법1/2156_포도주 시식.py
@@ -5,10 +5,10 @@ for i in range(n):
 
 dp = [0]
 dp.append(wine[0])
-if(n>1):
+if(n>1):                        
     dp.append(wine[0]+wine[1])
 
-for i in range(3,n+1):
+for i in range(3,n+1):                  # 3가지 경우 조건 따져서 대입
     case_1 = wine[i-1] + dp[i-2]
     case_2 = wine[i-1] + wine[i-2] + dp[i-3]
     case_3 = dp[i-1]
